3dd96d7bc701a7b6ed2c9073f1411b03d5626235
seri <seri.wb@gmail.com>
2022-08-03 00:00:28 +0900

    chore: add packages

--- apps/web/relay.config.js
+++ apps/web/relay.config.js
@@ -3,6 +3,6 @@ module.exports = {
   // Configuration options accepted by the `relay-compiler` command-line tool and `babel-plugin-relay`.
   src: "./src",
   language: "typescript",
-  schema: "./data/schema.graphql",
+  schema: "./src/graph/schema.graphql",
   exclude: ["**/node_modules/**", "**/__mocks__/**", "**/__generated__/**"],
 };
